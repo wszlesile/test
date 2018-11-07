b8a6127684b516f1386a3464da5e6f0e1db1bd6e
wangshuzheng <304297087@qq.com>
2018-11-07 13:24:30 +0800

    Init

---
+++ src/main/java/com/wsz/test/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.wsz.test.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/test")
+public class HelloController {
+    @RequestMapping("/hello")
+    @ResponseBody
+    public Map<String, Object> hello(){
+        Map<String,Object> result = new HashMap<>();
+        result.put("msg","hello");
+        return result;
+    }
+}
